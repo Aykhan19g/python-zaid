72eabe7d7cf6d19c2a0220ff4d61746ed52a673c
Aykhan Tanirverdiyev <127597877+Aykhan19g@users.noreply.github.com>
2024-08-18 16:45:07 +0400

    net-accepter.py

--- 6 netfilterqueue/net-accepter.py
+++ 6 netfilterqueue/net-accepter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python2.7
 #capture the request packet from client and save to a queue using iptables and alter send or recieve modified packet
-#iptables commands iptables -I FORWARD -j NFQUEUE --queue-num 0
+#iptables commands --> iptables -I FORWARD -j NFQUEUE --queue-num 0
 
 import netfilterqueue
 
 
 def process_packet(packet):
-	print packet
+	print (packet)
 	packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
